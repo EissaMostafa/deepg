5c8ccd91fd4848e3f662482c7c3cf85b052911e8
Milena Zurmuehl <milena_zurmuehl@gmx.net>
2023-02-22 20:57:25 +0100

    merge main to branch

--- deepsnake/lib/games.py
+++ deepsnake/lib/games.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 import time
-from deepsnake.cfg.default import Direction, DisplayConfig, GameConfig, GameStatus
+from deepsnake.cfg.default import Direction, DisplayConfig, GameConfig
 from collections import namedtuple
 
 Food = namedtuple("Food", "x y")
@@ -34,7 +34,6 @@ class SnakeGame:
             ),
         )
         self.direction = Direction.RIGHT
-        self.status = GameStatus.RUNNING
         self.clock = pygame.time.Clock()
         self._draw_display()
 
@@ -73,17 +72,14 @@ class SnakeGame:
     def _move_snake(self):
         h_x, h_y = self.snake[-1]
         self.snake = self.snake[1:]  # Remove last block (end of the snake tail)
-
         if self.direction == Direction.UP:
-            h_y -= self.display_cfg.block_size
+            self.snake.append((h_x, h_y - self.display_cfg.block_size))
         if self.direction == Direction.DOWN:
-            h_y += self.display_cfg.block_size
+            self.snake.append((h_x, h_y + self.display_cfg.block_size))
         if self.direction == Direction.LEFT:
-            h_x -= self.display_cfg.block_size
+            self.snake.append((h_x - self.display_cfg.block_size, h_y))
         if self.direction == Direction.RIGHT:
-            h_x += self.display_cfg.block_size
-
-        self.snake.append((h_x, h_y))
+            self.snake.append((h_x + self.display_cfg.block_size, h_y))
 
     def handle_key(self, key):
         if key == pygame.K_UP:
@@ -97,25 +93,5 @@ class SnakeGame:
 
     def play_step(self):
         self._move_snake()
-        self._check_game_over()
-
         self._draw_display()
         self.clock.tick(self.game_cfg.speed)
-
-    def _check_game_over(self):
-        h_x, h_y = self.snake[-1]
-        width_cond = (h_x < 0) or (h_x >= self.display_cfg.width)
-        height_cond = (h_y < 0) or (h_y >= self.display_cfg.height)
-        if width_cond or height_cond:
-            self.status = GameStatus.GAME_OVER
-
-    def _game_over(self):
-        # If game over is true, draw game over
-        text = pygame.font.SysFont("arial", 70).render(
-            "Game Over", True, (255, 255, 255), (0, 0, 0)
-        )
-        text_rect = text.get_rect()
-        text_x = self.display.get_width() / 2 - text_rect.width / 2
-        text_y = self.display.get_height() / 2 - text_rect.height / 2
-        self.display.blit(text, [text_x, text_y])
-        pygame.display.flip()
